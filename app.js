442a2ec76f88e109c84a33b731a0466c47e84751
Umang Mathur <umangmathur10051992@gmail.com>
2018-09-17 22:19:06 -0700

    fixes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV === 'development') {
+  require("dotenv").config();
+}
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -11,10 +15,6 @@ var usersRouter = require('./routes/users');
 var searchRouter = require('./routes/search');
 var deleteRouter = require('./routes/deletestudent');
 var addRouter = require('./routes/addnewstudent');
-
-if (process.env.NODE_ENV === 'development') {
-    require("dotenv").config();
-}
 
 var app = express();
 
@@ -52,7 +52,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = err;
 
   // render the error page
   res.status(err.status || 500);
--- db/index.js
+++ db/index.js
@@ -1,4 +1,3 @@
 const pgp = require('pg-promise')();
-require('dotenv').config();
 const dbConnection = pgp(process.env.DATABASE_URL);
 module.exports = dbConnection;
